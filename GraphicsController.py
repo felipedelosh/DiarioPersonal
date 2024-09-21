43523da817389f2be325f8b5c0e648069e72ebbc
felipedelosh <doctorfhernandez@hotmail.com>
2024-09-21 11:04:29 -0500

    Solve BUG feelings graphics

--- GraphicsController.py
+++ GraphicsController.py
@@ -16,57 +16,58 @@ class GraphicsController:
         {"data":frecuency, "data":frecuency, "data":frecuency ...}
         and draw
         """
-        pantalla = Toplevel()
-        pantalla.title(title)
-        pantalla.geometry("800x600")
-        tela = Canvas(pantalla, width=800, height=600, bg="snow")
-        tela.place(x=0, y=0)
-        # Screem values
-        _x = int(tela['width'])
-        _y = int(tela['height'])
-        _maxGrapichFrecuencyValueX = _x - _x*0.35 # This is a width of 100% line 0.2 + 0.15
-        _maxGrapichFrecuencyValueY = _y - _y*0.1  # This is a total heigth
-        _totalFeelingsSpace = _maxGrapichFrecuencyValueY / len(data)
-        _totalFeelingsSpace = _totalFeelingsSpace * 0.8
-        _xKons = _x*0.2 # The bit scale
+        if len(data) > 0:
+            pantalla = Toplevel()
+            pantalla.title(title)
+            pantalla.geometry("800x600")
+            tela = Canvas(pantalla, width=800, height=600, bg="snow")
+            tela.place(x=0, y=0)
+            # Screem values
+            _x = int(tela['width'])
+            _y = int(tela['height'])
+            _maxGrapichFrecuencyValueX = _x - _x*0.35 # This is a width of 100% line 0.2 + 0.15
+            _maxGrapichFrecuencyValueY = _y - _y*0.1  # This is a total heigth
+            _totalFeelingsSpace = _maxGrapichFrecuencyValueY / len(data)
+            _totalFeelingsSpace = _totalFeelingsSpace * 0.8
+            _xKons = _x*0.2 # The bit scale
 
-        # Search a max value in data
-        _maxYValue = 0
-        for i in data:
-            if _maxYValue < data[i]:
-                _maxYValue = data[i]
+            # Search a max value in data
+            _maxYValue = 0
+            for i in data:
+                if _maxYValue < data[i]:
+                    _maxYValue = data[i]
 
-        # Paint Axis XY
-        tela.create_line(_xKons, _y*0.1, _xKons + _maxGrapichFrecuencyValueX, _y*0.1, arrow=LAST) # X
-
-
-        # Paint scale lines:
-        _scaleX = (_maxGrapichFrecuencyValueX) / 10
-        for i in range(1, 10):
-            tela.create_line((_xKons+(_scaleX*i)),((_y*0.1)-5),(_xKons+(_scaleX*i)),((_y*0.1)+5))
-        # Paint medium and max values
-        tela.create_text(_xKons+(_scaleX*5), _y*0.075, text=str(round((_maxYValue/2), 2)))
-        tela.create_text(_xKons+(_scaleX*10), _y*0.075, text=str(_maxYValue))
+            # Paint Axis XY
+            tela.create_line(_xKons, _y*0.1, _xKons + _maxGrapichFrecuencyValueX, _y*0.1, arrow=LAST) # X
 
 
-        tela.create_line(_xKons, _y*0.1, _xKons, _maxGrapichFrecuencyValueY, arrow=LAST) # Y
+            # Paint scale lines:
+            _scaleX = (_maxGrapichFrecuencyValueX) / 10
+            for i in range(1, 10):
+                tela.create_line((_xKons+(_scaleX*i)),((_y*0.1)-5),(_xKons+(_scaleX*i)),((_y*0.1)+5))
+            # Paint medium and max values
+            tela.create_text(_xKons+(_scaleX*5), _y*0.075, text=str(round((_maxYValue/2), 2)))
+            tela.create_text(_xKons+(_scaleX*10), _y*0.075, text=str(_maxYValue))
 
-        # Paint Labels
-        counter = 1
-        for i in data:
-            # Paint feeling Label
-            yPos = (_totalFeelingsSpace*counter)+_y*0.1
-            tela.create_text(_x * 0.07, yPos, text=i)
 
-            _percentValueFrecuency = data[i]/_maxYValue
-            # Paint frecuency 
-            x0 = _xKons
-            y0 = yPos - 5
-            x1 = _xKons + (_maxGrapichFrecuencyValueX * _percentValueFrecuency)
-            y1 = yPos + 5
-            tela.create_rectangle(x0, y0, x1, y1, fill="blue")
+            tela.create_line(_xKons, _y*0.1, _xKons, _maxGrapichFrecuencyValueY, arrow=LAST) # Y
 
-            counter = counter + 1
+            # Paint Labels
+            counter = 1
+            for i in data:
+                # Paint feeling Label
+                yPos = (_totalFeelingsSpace*counter)+_y*0.1
+                tela.create_text(_x * 0.07, yPos, text=i)
+
+                _percentValueFrecuency = data[i]/_maxYValue
+                # Paint frecuency 
+                x0 = _xKons
+                y0 = yPos - 5
+                x1 = _xKons + (_maxGrapichFrecuencyValueX * _percentValueFrecuency)
+                y1 = yPos + 5
+                tela.create_rectangle(x0, y0, x1, y1, fill="blue")
+
+                counter = counter + 1
 
 
     def graphEconomyBoxStates(self, canvas, dataLitleBox, dataBigBox):
